1c550d98373b4d63a465ccf85e36f84a70540ff4
tans.tan <tans.tan096@gmail.com>
2024-03-30 02:29:56 +0800

    Remove demo plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.tansDemo)
+    //alias(libs.plugins.tansDemo)
 }
 
 android {
